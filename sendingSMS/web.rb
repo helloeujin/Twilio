777f0aff2f45c60de6d4487e125f2392ecf9a535
Youjin Shin <meggonagul@gmail.com>
2014-01-27 17:13:37 -0500

    adding new stuff

---
+++ sendingSMS/web.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+require 'rubygems' # not necessary with ruby 1.9 but included for completeness
+require 'twilio-ruby'
+
+# To find these visit https://www.twilio.com/user/account
+
+account_sid = "AC057a2d8192eae97fdafe9dbc6c688dc6"
+auth_token = "086057d5fc72ca6b195f967fce2ba375"
+
+@client = Twilio::REST::Client.new account_sid, auth_token
+
+@message = @client.account.messages.create({
+                                  #:to => "+19178685970",
+                                  :to => "+16467172770",
+                                  :from => "+13479605166",
+                                  :body => "Hi!"})
+
+get '/' do
+  "BaBoYa3"
+end
